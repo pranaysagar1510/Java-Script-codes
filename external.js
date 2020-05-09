39543731e667e29289bf559d36e4992ed126fcfd
Pranay Sagar <65065949+pranaysagar1510@users.noreply.github.com>
2020-05-09 23:07:24 +0530

    Add files via upload

---
+++ external.js
@@ -0,0 +1,7 @@
+ /*
+ document.write("I am giving demonstration on multiline comments");
+ document.write("I am giving demonstration on multiline comments");
+ */
+ 
+document.write("Hello Guys My Name is  Pranay Sagar</br>");
+
